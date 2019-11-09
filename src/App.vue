74de24cb7a434df5a5a24ab14be213d7353b3832
sherkhan <sherkhan@xakep.ru>
2019-11-09 22:17:04 +0300

    add alice calc

--- src/App.vue
+++ src/App.vue
@@ -28,14 +28,15 @@
       <el-col :span="6"> <el-button @click.stop.prevent="recalculateAmounts(1000)">Add 1,000 EOS</el-button></el-col>
       <el-col :span="6"><el-button @click.stop.prevent="recalculateAmounts(10000)">Add 10,000 EOS</el-button></el-col>
       <el-col :span="6"><el-button @click.stop.prevent="recalculateAmounts(100000)">Add 100,000 EOS</el-button></el-col>
-      <el-col :span="6"><el-input v-model.number="extra.eos" @blur="recalculateAmounts(extra.eos)" placeholder="on blur recalculation"></el-input></el-col>
+      <el-col :span="6"><el-button @click.stop.prevent="recalculateAmounts(1000000)">Add 1,000,000 EOS</el-button></el-col>
+      
   </el-row>
 
   </div>
   <br>
   <el-row :gutter="20">
-      <el-col :span="6"><el-button @click.stop.prevent="autoRecalculation(1000000)">Auto untill 1kk</el-button></el-col>
-      <el-col :span="6"><el-button @click.stop.prevent="autoRecalculation(3000000)">Auto untill 3kk</el-button></el-col>
+      <el-col :span="6"><el-input v-model.number="extra.eos" placeholder="Enter Amount"></el-input></el-col>
+      <el-col :span="6"><el-button @click.stop.prevent="autoRecalculation(extra.eos)">Auto untill {{extra.eos ? extra.eos : 'input amount'}}</el-button></el-col>
       <el-col :span="6"><el-button @click.stop.prevent="autoRecalculation(5000000)">Auto untill 5kk</el-button></el-col>
       <el-col :span="6"><el-button @click.stop.prevent="autoRecalculation(8000000)">Auto untill 8kk</el-button></el-col>
   </el-row>
@@ -108,9 +109,84 @@
         <div  class="text item">
           {{extra.dividends }}
         </div>
+        </el-card>
+        <el-card>
+        <div slot="header" class="clearfix">
+          <span>Your Referal Bonus</span>
+        </div>
+        <div  class="text item">
+          {{extra.refs }}
+        </div>
       </el-card>
       </el-col>
     </el-row>
+  <br>
+  <el-row :gutter="20">
+    <el-form>
+    <el-col :span="6">
+      <el-form-item label="Enter Friends Amount">
+      <el-input-number v-model.number="alice.friends" placeholder="Enter Friends Amount" :min="1" :max="100"></el-input-number>
+      </el-form-item>
+    </el-col>
+    <el-col :span="6">
+      <el-form-item label="Enter Friend min Deposit">
+      <el-input-number v-model.number="alice.min" placeholder="Enter Friend min Deposit" :min="100" :max="1000"></el-input-number>
+      </el-form-item>
+    </el-col>
+    <el-col :span="6">
+      <el-form-item label="Enter Friend max Depost">
+      <el-input-number v-model.number="alice.max" placeholder="Enter Friend max Depost" :min="1000" :max="10000"></el-input-number>
+      </el-form-item>
+    </el-col>
+    <el-col :span="6">
+      <el-form-item label="Alice Amount">
+       <el-input-number v-model.number="alice.amount" placeholder="Alice Amount"></el-input-number>
+      </el-form-item>
+    </el-col>
+    </el-form>
+  </el-row>
+  <br>
+  <el-row>
+    <el-col>
+      <el-button @click="linkFriends(alice)" :disabled="!alice.friends && !alice.min && !alice.max && !alice.amount">Let's Alice buy on {{alice.amount}}EOS and link Her Friends</el-button>
+    </el-col>
+  </el-row>
+  <br>
+  <el-row>
+    <el-col>
+      <el-card>
+        <div slot="header" class="clearfix">
+          Alice
+        </div>
+        <h4 v-if="alice.pow">Alice bought <strong>{{alice.pow.toFixed(0)}}</strong>POW for <strong>{{alice.amount}}</strong>EOS and the price was <strong>{{alice.price}}</strong></h4>
+        <p v-if="alice.persantage">Alice participation is {{alice.persantage}}%</p>
+        <p v-if="alice.refs">Alice referals bonus is {{alice.refs.toFixed(0)}}EOS</p>
+        <p v-if="alice.dividends">Alice dividends is {{alice.dividends.toFixed(0)}}EOS</p>
+        <p v-if="alice.pow">Alice can sell her <strong>{{alice.pow.toFixed(0)}}</strong>POW for {{this.price.sell}} price and get  {{alice.pow * this.price.sell}}EOS</p>
+        <p v-if="alice.pow">At the end Alice will own {{(alice.pow * this.price.sell) + alice.refs + alice.dividends}}EOS</p>
+
+      </el-card>
+    </el-col>
+  </el-row>
+  <el-row v-if="showFriendsDep">
+    <el-col>
+       <el-table
+      :data="friends"
+      style="width: 100%">
+      <el-table-column
+        prop="name"
+        label="Name"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="amount"
+        label="Amount (EOS)"
+        width="180">
+      </el-table-column>
+    </el-table>
+    </el-col>
+  </el-row>
+   
 
   </div>
 </template>
@@ -120,12 +196,15 @@
 import _get from 'lodash/get'
 import _head from 'lodash/head'
 import _sum from 'lodash/sum'
+import { RANDOM_NAMES } from './names'
 
 const URL = 'https://eosapi.blockmatrix.network/v1/chain/get_table_rows'
 
-const requester = {
+const ACCOUNT = 'vladendark35'
+
+const REQUESTER = {
   contract: JSON.stringify({json: true, code:'powhcontract', table: 'stat', scope:'POW'}),
-  account: JSON.stringify({json: true, code:'powhcontract', table: 'accounts', scope:'vladendark35'}),
+  account: JSON.stringify({json: true, code:'powhcontract', table: 'accounts', scope: ACCOUNT}),
   token: JSON.stringify({json: true, code:'eosio.token', table: 'accounts', scope:'powhcontract'})
 }
 
@@ -134,6 +213,18 @@ export default {
   name: 'app',
   data (){
     return {
+      alice: {
+        friends: 10,
+        min: 100,
+        max: 1000,
+        amout: null,
+        pow: null,
+        price: null,
+        refs: null,
+        dividends: null,
+        persantage: null
+      },
+      friend: [],
       price: {
         buy: null,
         sell: null,
@@ -153,8 +244,10 @@ export default {
       extra: {
         eos: null,
         pow: null,
-        dividends: 0
-      }
+        dividends: 0,
+        refs: 0
+      },
+      showFriendsDep: false
     }
   },
   computed: {
@@ -173,7 +266,48 @@ export default {
   },
   methods: {
     getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min) ) + min;
+      return Math.floor(Math.random() * (max - min) ) + min;
+    },
+
+    prepareAliceFriendsArray (alice) {
+      let friendsArr = []
+      let index = 0
+      while (index < alice.friends) {
+        friendsArr.push({
+          isRef: true,
+          name: RANDOM_NAMES[index],
+          amount: this.getRndInteger(alice.min, alice.max)
+        })
+        index ++
+      }
+      return friendsArr
+    },
+
+    calculateAlicePercentage() {
+      return ((this.alice.pow / this.contract.currentSupply.number) * 100).toFixed(2)
+    },
+    calculateAliceDicidends(amount) {
+        return
+    },
+    calculateAlicesFriendsTransactions (friends) {
+        friends.forEach((friend) => {
+          this.recalculateAmounts(friend.amount, friend.isRef)
+          this.alice.persantage = this.calculateAlicePercentage()
+          this.alice.dividends = this.alice.dividends + ((((friend.amount / 100) * 7) * this.alice.persantage ) / 100)
+        })
+    },
+
+    async linkFriends(alice) {
+      this.friends = await this.prepareAliceFriendsArray(alice)
+      /*alive bought*/
+      this.alice.price = this.price.buy
+      this.alice.pow = this.alice.amount / this.price.buy
+      this.recalculateAmounts(alice.amount)
+      this.calculateAlicesFriendsTransactions(this.friends)
+      this.showFriendsDep = true
+      
+
+      
     },
 
     autoRecalculation (summ) {
@@ -192,8 +326,8 @@ export default {
         medium: initialPrice
       }
     },
-    recalculateAmounts (extra) {
-      this.addDividends(extra)
+    recalculateAmounts (extra, ref) {
+      this.addDividends(extra, ref)
       let extraPow = extra / this.price.buy
       this.contract.currentSupply.number = this.contract.currentSupply.number + extraPow
       this.contract.eos = this.contract.eos + extra
@@ -201,8 +335,13 @@ export default {
       
       return 
     },
-    addDividends (amount) {
+    addDividends (amount, ref) {
         this.extra.dividends = this.extra.dividends + ((((amount / 100) * 7) * this.yourPowPersentage) / 100)
+       
+       /*Alice calculation Only*/
+        if (ref) {
+          this.alice.refs = this.alice.refs + ((amount / 100) * 3)
+        }
     },
     async requestBlockData (params) {
       return await this.axios.post(URL, params)
@@ -223,9 +362,12 @@ export default {
     }
   },
   async mounted () {
+    
+    this.alice.amount = this.getRndInteger(100,1000)
+    
     setTimeout(async () => {
-      for (let item in requester) {
-      await this.getBlockData(item, requester[item])
+      for (let item in REQUESTER) {
+      await this.getBlockData(item, REQUESTER[item])
     }
     this.price = Object.assign({}, this.price, await this.calculatePrice(this.contract.currentSupply.number, this.contract.maxSupply))
     }, 500)
@@ -249,5 +391,8 @@ export default {
   button {
     display: block;
     width: 100%;
+  }
+  .red {
+    color: red
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { Button, Select, Row, Col, Input, Container, Form, FormItem, Card } from 'element-ui';
+import { Button,
+  Select, 
+  Row,
+  Col,
+  Input,
+  InputNumber,
+  Container,
+  Form, 
+  FormItem, 
+  Card,
+  Table,
+TableColumn} from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import axios from 'axios'
 import VueAxios from 'vue-axios'
@@ -13,10 +24,13 @@ Vue.use(Select)
 Vue.use(Row)
 Vue.use(Col)
 Vue.use(Input)
+Vue.use(InputNumber)
 Vue.use(Container)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Card)
+Vue.use(Table)
+Vue.use(TableColumn)
 
 Vue.use(VueAxios, axios)
 
